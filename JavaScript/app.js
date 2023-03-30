fdf0e25afd69135087411ff91b8d8ea3f28ae794
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-03-30 10:13:40 +0530

    Basic JavaScript

---
+++ JavaScript/app.js
@@ -0,0 +1,7 @@
+console.log("Hello Bhai sab Kya haal chaal?")
+let effort = 100;
+let time = 3;
+let placement = (effort*time)/2;
+console.log(placement);
+alert("Bhai yaha kyu aaya hai!!")
+let ans = prompt("kitne ka package chahiye? bta bta")
