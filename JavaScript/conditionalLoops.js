baddc2a0c976af32ae0118b6ceb99af3751e63f1
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-03-30 10:13:52 +0530

    Conditional and Loops

---
+++ JavaScript/conditionalLoops.js
@@ -0,0 +1,11 @@
+console.log("check check check");
+let age = 20;
+if(age <= 18){
+    console.log("Teenage");
+}
+else if(age===20){
+    console.log("Perfect age");
+}
+else{
+    console.log("Buddhe");
+}
