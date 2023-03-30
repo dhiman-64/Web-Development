2e3cee09fcdc5bb5e3b5149662ab0b5a0f8ccc17
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-03-30 18:20:32 +0530

    BASICS

---
+++ JavaScript/basic.js
@@ -0,0 +1,46 @@
+let age = 20;
+age = age+1;
+console.log(age);
+
+const firstName = "Ikshit"
+const lastName = "Dhiman"
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+let num1 = 10;
+let num2 = 20;
+
+// Arithmetic Operations
+let add = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1/num2;
+let modulo = num1 % num2;
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(modulo);
+console.log(0);
+
+// Relational Operators
+
+let com1 = num1 > num2;
+let com2 = num1 < num2;
+let com3 = num1 == num2;
+let com4 = num1 === num2;
+let com5 = num1 != num2;
+let com6 = num1 <= num2;
+let com7 = num1 >= num2;
+let com8 = num1 !== num2;
+
+console.log(com1);
+console.log(com2);
+console.log(com3);
+console.log(com4);
+console.log(com5);
+console.log(com6);
+console.log(com7);
+console.log(com8);
