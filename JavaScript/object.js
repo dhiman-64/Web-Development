19a911bdb10d04fe343fdd2719eec4fcc3ed5532
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-01 09:44:08 +0530

    Objects JavaScript

---
+++ JavaScript/object.js
@@ -0,0 +1,12 @@
+const fitBitData = {
+    totalSteps : 308907,
+    totalMiles : 211.7,
+    avgCalorieBurn : 5744,
+    workoutsThisWeek : '5 of 7',
+    avgGoodSleep : '2:13',
+    sportsMode : ['Cycling','Running','Streching'],
+    isEmergency : false
+};
+
+let a1 = fitBitData[totalMiles];
+console.log(fitBitData);
